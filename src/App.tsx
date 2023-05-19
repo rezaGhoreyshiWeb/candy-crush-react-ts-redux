b561667f43b23e744949b9c429a933ad160c521c
rezaGhoreyshiWeb <rezaghoreyshi.contact@gmail.com>
2023-05-19 06:10:52 +0330

    Board & Tile

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "./store/hooks";
 import { updateBoard } from "./store";
 import { createBoard } from "./utils/createBoard";
+import Board from "./components/Board";
 
 function App() {
   const dispatch = useAppDispatch();
@@ -14,7 +15,11 @@ function App() {
     dispatch(updateBoard(createBoard(boardSize)));
   }, [boardSize, dispatch]);
 
-  return <div>app</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Board />
+    </div>
+  );
 }
 
 export default App;
