1b4944ca8908278b4a331f4df561cd706d575b33
rezaGhoreyshiWeb <rezaghoreyshi.contact@gmail.com>
2023-05-19 05:52:24 +0330

    updateBoard & createBoard

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import { updateBoard } from "./store";
+import { createBoard } from "./utils/createBoard";
 
 function App() {
+  const dispatch = useAppDispatch();
+  const board = useAppSelector(({ candyCrush: { board } }) => board);
+  const boardSize = useAppSelector(
+    ({ candyCrush: { boardSize } }) => boardSize
+  );
 
+  useEffect(() => {
+    dispatch(updateBoard(createBoard(boardSize)));
+  }, [boardSize, dispatch]);
 
-  return (
-    <>
-      <div>
-      
-      </div>
-     
-    </>
-  )
+  return <div>app</div>;
 }
 
-export default App
+export default App;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,26 +1,30 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, configureStore, createSlice } from "@reduxjs/toolkit";
 
-const initialState : {
-    board: string[];
-    boardSize: number;
+const initialState: {
+  board: string[];
+  boardSize: number;
 } = {
-    board: [],
-    boardSize: 8,
+  board: [],
+  boardSize: 8,
 };
 
 const candyCrushSlice = createSlice({
-    name: "candyCrush",
-    initialState,
-    reducers: {
-
-    }
+  name: "candyCrush",
+  initialState,
+  reducers: {
+    updateBoard: (state, action: PayloadAction<string[]>) => {
+        state.board = action.payload;
+    },
+  },
 });
 
 export const store = configureStore({
-    reducer: {
-        candyCrush : candyCrushSlice.reducer,
-    }
+  reducer: {
+    candyCrush: candyCrushSlice.reducer,
+  },
 });
+
+export const  { updateBoard} = candyCrushSlice.actions;
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
